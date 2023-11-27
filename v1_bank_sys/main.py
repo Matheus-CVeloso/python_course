1fc51aaf3ef5815b9b6a8901b55b85e78fd91157
Matheus <matheuscoelhoveloso@gmail.com>
2023-11-27 14:26:34 -0300

    inserted pattern into code

--- v1_bank_sys/main.py
+++ v1_bank_sys/main.py
@@ -12,9 +12,9 @@ while True:
       [E] - Extrato
       [Q] - Sair
     ''')
-    opcao = input("Digite um número com a opção escolhida: ")
+    option = input("Digite um número com a opção escolhida: ")
 
-    if (opcao.upper() == 'S'):
+    if (option.upper() == 'S'):
         amount_withdraw = int(input("Quanto você deseja sacar? "))
         withdraw_money_operation = withdraw_money.withdraw_money(
             balance, amount_withdraw, 500, extract)
@@ -22,16 +22,16 @@ while True:
         extract.append(withdraw_money_operation[1])
         print(balance)
 
-    elif (opcao.upper() == 'D'):
+    elif (option.upper() == 'D'):
         amount_to_deposit = int(input("Quanto você deseja depositar? "))
         deposit_operation = (deposit.deposit(extract, amount_to_deposit))
         balance += deposit_operation[0]
         extract.append(deposit_operation)
         print(balance)
 
-    elif (opcao.upper() == 'E'):
+    elif (option.upper() == 'E'):
         print(extract)
-    elif (opcao.upper() == 'Q'):
+    elif (option.upper() == 'Q'):
         print("Até mais...")
         sys.exit()
     else:
